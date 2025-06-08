b86a57ff31a999a58e25f5534ebae5a47bfb4c79
Ayush Ranjan <codingwithayush10@gmail.com>
2025-06-08 12:36:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Enable CORS for your frontend origin
 app.use(cors({
-  origin: 'https://bro-room-1.onrender.com',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST'],
 }));
 
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'https://bro-room-1.onrender.com',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST'],
   }
 });
