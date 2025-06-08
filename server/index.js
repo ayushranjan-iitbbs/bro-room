26c4b9f6f03e74dfd9aa1e18b5cf2ea00618d8f5
Ayush Ranjan <codingwithayush10@gmail.com>
2025-06-08 12:15:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000; 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
